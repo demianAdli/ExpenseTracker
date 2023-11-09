42a26936f8ae68992af6a269120a2061bd5325e2
Alireza Adli <alireza.adli@mail.concordia.ca>
2023-11-09 18:12:06 -0500

    Add ExpenseList

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 import { useState } from "react";
-import Form from "./components/Form.tsx";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
   return (
     <div>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
