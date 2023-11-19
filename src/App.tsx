18ea97adb96cc1175eb27c862e43a3fd53f1de88
Alireza Adli <alireza.adli@mail.concordia.ca>
2023-11-19 11:01:42 -0500

    Export a const when you want others to use it!

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import { useState } from "react";
+
+export const categories = ["Groceries", "Utilities", "Entertainment"];
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -16,6 +19,9 @@ function App() {
     : expenses;
   return (
     <div>
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
       <div className="mb-3">
         <ExpenseFilter
           onSelectCategory={(category) => setSelectedCategory(category)}
